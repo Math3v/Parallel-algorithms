ed5b057805091e45639ef3e09bd7a83a0c4c081a
Matej Minarik <matejminarik.fiit@gmail.com>
2015-03-18 13:22:58 +0100

    Run script updated

--- pms/pms.sh
+++ pms/pms.sh
@@ -5,17 +5,9 @@ F_NUMPROC=`echo "l($1)/l(2)" | bc -l`
 NUMPROC=${F_NUMPROC%.*}
 ((NUMPROC++))
 
-#echo "Starting test with $1 values and $NUMPROC processors"
-
-
 mpic++ --prefix /usr/local/share/OpenMPI -g -o pms pms.cpp
-
-START=$(($(date +%s%N)/1000000))
 mpirun --prefix /usr/local/share/OpenMPI -np $NUMPROC pms
 #mpirun --prefix /usr/local/share/OpenMPI -np $NUMPROC -n $NUMPROC xterm -e gdb -q -tui -x gdb.txt ./pms
-END=$(($(date +%s%N)/1000000))
-
-#echo "Time: `expr $END - $START`"
 
 rm -f pms
 rm -f numbers
