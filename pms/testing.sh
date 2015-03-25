796909644214c49a7132001a3c6c3272d517e03a
Matej Minarik <matejminarik.fiit@gmail.com>
2015-03-25 12:04:51 +0100

    Maximum power of two as argument

--- pms/testing.sh
+++ pms/testing.sh
@@ -5,7 +5,15 @@ RES=0
 TOTAL=0
 TESTRUN=100
 
-for i in {2..14}
+if (($# != 1))
+then
+	echo "Error: Please specify maximum power of 2"
+	exit 1
+fi
+
+MAX=$1
+
+for (( i=1; i<=$MAX; i++))
 do
 	RES=`echo "2^$i" | bc`
 	echo "$RES"
