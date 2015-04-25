5f490b563219f7d58ab9894c11f92c460270dae5
Matej Minarik <matejminarik.fiit@gmail.com>
2015-04-25 15:04:42 +0200

    Added debugging capabilities

--- mm/test.sh
+++ mm/test.sh
@@ -5,6 +5,7 @@ mat2=$(head -n1 mat2)
  
 cpus=$((mat1*mat2))
  
-mpic++ --prefix /usr/local/share/OpenMPI -o mm mm.cpp -std=c++0x
+mpic++ --prefix /usr/local/share/OpenMPI -g -o mm mm.cpp -std=c++0x
 mpirun --prefix /usr/local/share/OpenMPI -np $cpus mm
+#mpirun --prefix /usr/local/share/OpenMPI -np $cpus -n $cpus xterm -e gdb -q -tui -x gdb.txt ./mm
 rm -f mm
